0ab7076dfb3fe8d37043b604f0bc284714c20015
kk <kazdovak@gmail.com>
2026-07-30 16:23:45 +0200

    edit player and enemy animation

--- vampires-clone/Scripts/player.gd
+++ vampires-clone/Scripts/player.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
 
 var movement_speed = 40.0
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
 
 func _physics_process(delta):
 	movement()
@@ -9,5 +11,11 @@ func movement():
 	var x_mov = Input.get_action_strength("right") - Input.get_action_strength("left")
 	var y_mov = Input.get_action_strength("down") - Input.get_action_strength("up")
 	var mov = Vector2(x_mov,y_mov)
+	
+	if x_mov > 0:
+		animated_sprite_2d.flip_h = false
+	elif x_mov < 0:
+		animated_sprite_2d.flip_h = true
+	
 	velocity = mov.normalized()*movement_speed
 	move_and_slide()
