86a5bc794dda532c87a7ad7f26a653b916807bb3
kk <kazdovak@gmail.com>
2026-08-11 18:33:04 +0200

    edit death

--- vampires-clone/Scenes/Enemy/death.gd
+++ vampires-clone/Scenes/Enemy/death.gd
@@ -1,6 +1,7 @@
 extends AnimatedSprite2D
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	$AnimatableSprite2D.play("death")
+	play("death")
+
+func _on_animation_finished() -> void:
+	queue_free()
--- vampires-clone/Scripts/enemy.gd
+++ vampires-clone/Scripts/enemy.gd
@@ -9,6 +9,8 @@ var knockback = Vector2.ZERO
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var snd_hit = $snd_hit
 
+var death_anim = preload("res://Scenes/Enemy/death.tscn")
+
 
 signal remove_from_array(object)
 
@@ -23,14 +25,19 @@ func _physics_process(_delta):
 		animated_sprite_2d.flip_h = false
 	elif direction.x < -0.1:
 		animated_sprite_2d.flip_h = true
-		
 
+func death():
+	emit_signal("remove_from_array",self)
+	var enemy_death = death_anim.instantiate()
+	enemy_death.scale = animated_sprite_2d.scale
+	enemy_death.global_position = global_position
+	get_parent().call_deferred("add_child", enemy_death)
+	queue_free()
 
 func _on_hurt_box_hurt(damage, angle, knockback_amount):
 	hp -= damage
 	knockback = angle * knockback_amount
 	if hp <= 0:
-		emit_signal("remove_from_array", self)
-		queue_free()
+		death()
 	else:
 		snd_hit.play()
